02eeee63527eed40adc16338f8c48d8e1fe7ce95
stefanobini28 <74369128+stefanobini28@users.noreply.github.com>
2021-01-15 17:03:08 +0100

    Update List.h

--- List.h
+++ List.h
@@ -1,5 +1,5 @@
 //
-// Created by betty on 07/01/2021.
+// Created by stefano on 08/01/2021.
 //
 
 #ifndef EXAMPLETXTTOLIST_LIST_H
@@ -7,27 +7,21 @@
 
 #include<list>
 using namespace std;
-//#include "global.h"
-//#include "edgelist.h"
-//struct ListNode {
 class ListItem{
 public:
     ListItem();
     ListItem(string itemName, int day, int month, int year, string description, string done, ListItem *next);
     ~ListItem();
     string name;
-    //int date [3] {day, month, year};
     int day, month, year;
     string description;
     string done;
 
     ListItem *next;
 
-// Setter
     void setIdentifier(int i) {
         identifier = i;
     }
-    // Getter
     int getIdentifier() const {
         return identifier;
     }
