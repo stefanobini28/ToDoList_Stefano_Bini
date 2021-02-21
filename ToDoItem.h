c8cefc323aa3978d32e1582b5b0bee804faf0336
stefanobini28 <74369128+stefanobini28@users.noreply.github.com>
2021-02-21 19:28:38 +0100

    Update ToDoItem.h

--- ToDoItem.h
+++ ToDoItem.h
@@ -2,29 +2,28 @@
 // Created by Stefano on 08/01/2021.
 //
 
-#ifndef EXAMPLETXTTOLIST_TODOITEM_H
-#define EXAMPLETXTTOLIST_TODOITEM_H
+#ifndef EXAMPLETXTTOMAP_TODOITEM_H
+#define EXAMPLETXTTOMAP_TODOITEM_H
 
 #pragma once
-#include "Date.h"
+//#include "Date.h"
 #include <utility>
+#include <vector>
 using namespace std;
 class ToDoItem{
 public:
     ToDoItem();
     //ToDoItem(string itemName, int day, int month, int year, string description, bool done);
-    ToDoItem(string itemName, Date date, string description, bool done);
+    ToDoItem(string itemName, /*Date date,*/ string description, bool done);
     ~ToDoItem();
-
-    Date date;
 
     void setName(string itemName) {
         name= std::move(itemName);
     }
-    string getName(){
+    string getName() const{
         return name;
     }
-    void setdDescription(string itemDescription) {
+    void setDescription(string itemDescription) {
         description= std::move(itemDescription);
     }
     string getDescription() const {
@@ -36,17 +35,19 @@ public:
     bool getItemDone() const {
         return done;
     }
-    bool setDate(int day, int month,int year);
+    //bool setDate(int day, int month,int year);
 
-    Date getDate() const;
+    //Date getDate() const;
+
+    //void showDate() const;
 
     friend ostream &operator<<(ostream &ostr, const ToDoItem &);
 
 private:
     string name;
-    //int day, month, year;
+    //Date date;
     string description;
     bool done;
 };
 
-#endif //EXAMPLETXTTOLIST_TODOITEM_H
+#endif //EXAMPLETXTTOLMAP_TODOITEM_H
