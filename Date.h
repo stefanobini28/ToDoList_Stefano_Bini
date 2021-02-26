07f0ec6ac48065930728c695b9051d077a6b40ef
stefanobini28 <74369128+stefanobini28@users.noreply.github.com>
2021-02-26 18:56:06 +0100

    Update Date.h

--- Date.h
+++ Date.h
@@ -9,27 +9,24 @@
 #include <string>
 #include <utility>
 #include <vector>
+#include <iostream>
+#include <sstream>
+
 using namespace std;
 
 class Date
 {
 public:
     Date();
-    Date(int year, int month, int day);
 
     string printDate() const;
     static bool dateValidation(int Year, int Month, int Day);
 
     void setDay(int itemDay);
-    int getDay() const;
     void setMonth(int itemMonth);
-    int getMonth() const;
     void setYear(int itemYear);
-    int getYear() const;
 
     bool setDate(int year, int month, int day);
-
-    Date getDate() const;
 
 private:
     int year;
