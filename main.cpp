ffa6d96aa2e0ea884ea8086863b5f989667387be
stefanobini28 <74369128+stefanobini28@users.noreply.github.com>
2021-04-07 19:35:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,13 @@
 #include <sstream>
 #include <algorithm>
 #include <limits>
-#include "Map.h"
+#include "ToDoList.h"
+
+#if defined(_WIN32)
+    #define PAUSE "pause"
+#elif defined(unix) || defined (__unix__) || defined(__unix)
+    #define PAUSE "read -p 'Press Enter to Continue...' var"
+#endif
 
 using namespace std;
 
@@ -53,14 +59,14 @@ int main() {
     char nome[20], desc[100];
     bool execute = true, success, validDate;
     string name, deleteName, checkName, description;
-    Map newMap;
+    ToDoList newMap;
 
     cout << "Welcome to To-Do list program!" << endl;
     cout << "Loading data from the external file..."<< endl;
     newMap.readFromFile();  //lettura da file di testo della lista salvata in precedenza
 
     do {
-        system("pause");
+        system(PAUSE);
         int action = menu();
         cin.ignore(256, '\n');
 
@@ -112,7 +118,11 @@ int main() {
                         if (!cin.fail())
                             break;
                     }
-                    validDate=Date::dateValidation(year,month,day);
+                    try {
+                        validDate = Date::dateValidation(year, month, day);
+                    }catch (exception & alert){
+                        cerr << alert.what() << endl;
+                    };
                 }
                 cout << "Enter description[max 100 characters]:";
                 cin.ignore(256, '\n');
@@ -120,32 +130,38 @@ int main() {
                 description = desc;
                 newMap.addItem(name,day,month,year,description,false); //aggiunta di un elemento d parte dell'utente
                 break;
-            case 3:
+            case 3:{
                 cout << "Insert the name of the event you want to delete:" << endl;
-                while (!success) {
+                do {
                     cin.getline(nome, sizeof(nome) + 1);
                     deleteName = nome;
-                    success = newMap.deleteElement(deleteName);  //rimuovere un evento dalla lista
-                    if (success)
+                    success = newMap.deleteElement(deleteName);//rimuovere un evento dalla lista
+                    if(deleteName == "back")
+                        break;
+                    if (success){
                         cout << "The element has been deleted successfully" << endl;
-                    else
-                        cout << "can't find the element, please retry" << endl;
-                }
+                    } else {
+                        cout << "can't find the element, please retry or digit 'back' to go to the main menu" << endl;
+                    }
+                } while (!success);
                 break;
-            case 4:
+            }
+            case 4: {
                 cout << "write here the name of the event you want to change [DONE <-> UNDONE]:" << endl;
-                while (!success) {
+                do {
                     cin.getline(nome, sizeof(nome) + 1);
                     checkName = nome;
-                    success = newMap.checkItem(
-                            checkName);  //contrassegnare un evento della todolist come fatto/non fatto
-
-                    if (success)
+                    if(checkName == "back")
+                        break;
+                    success = newMap.checkItem(checkName);  //contrassegnare un evento della todolist come fatto/non fatto
+                    if (success) {
                         cout << "The element has been changed successfully" << endl;
-                    else
-                        cout << "can't find the element, please retry:" << endl;
-                }
+                    } else {
+                        cout << "can't find the element, please retry or digit 'back' to go to the main menu" << endl;
+                    }
+                } while (!success);
                 break;
+            }
             case 5:
                 cout << "Have a nice day!" << endl;
                 newMap.writeOnFile();   //scrittura sul file di testo della lista aggiornata
@@ -155,6 +171,7 @@ int main() {
                 cout << "Input was not recognized. Try again or "
                      << "\nenter 'exit' to end the program." << endl;
                 execute = true;
+                break;
         }
     } while (execute);
     return 0;
