384581931eefa2e689f9e015cb4efe5fe178de23
stefanobini28 <74369128+stefanobini28@users.noreply.github.com>
2021-04-07 19:33:41 +0200

    Update and rename Map.h to ToDoList.h

--- ToDoList.h
+++ ToDoList.h
@@ -1,11 +1,3 @@
-//
-// Created by Stefano on 02/02/2021.
-//
-
-#ifndef EXAMPLETXTTOMAP_MAP_H
-#define EXAMPLETXTTOMAP_MAP_H
-
-#pragma once
 #include <iostream>
 #include <map>
 #include <algorithm>
@@ -13,18 +5,15 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <stdexcept>
+#include <exception>
 #include "ToDoItem.h"
 #include "Date.h"
 
 using namespace std;
 
-class Map {
+class ToDoList {
 public:
-    Map();
-    ~Map();
-
-    ToDoItem item;
-    Date date;
 
     void showMap() const ;
     void addItem (const string& name, int day, int month, int year, const string& description, bool done);
@@ -36,8 +25,6 @@ public:
 
 private:
     multimap<string,ToDoItem> todomap;
-    multimap<string,ToDoItem>::iterator iterator;
 };
 
-#endif //EXAMPLETXTTOMAP_MAP_H
-
+#endif //EXAMPLETXTTOMAP_TODOLIST_H
