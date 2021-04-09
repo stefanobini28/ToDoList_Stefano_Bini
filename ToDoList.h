fd88ddd3f50ca4ce0c3fc8812c2112b7dbf76369
stefanobini28 <74369128+stefanobini28@users.noreply.github.com>
2021-04-09 18:48:24 +0200

    Update ToDoList.h

--- ToDoList.h
+++ ToDoList.h
@@ -1,3 +1,11 @@
+//
+// Created by Stefano on 02/02/2021.
+//
+
+#ifndef EXAMPLETXTTOMAP_TODOLIST_H
+#define EXAMPLETXTTOMAP_TODOLIST_H
+
+#pragma once
 #include <iostream>
 #include <map>
 #include <algorithm>
@@ -15,7 +23,7 @@ using namespace std;
 class ToDoList {
 public:
 
-    void showMap() const ;
+    string showMap() const;
     void addItem (const string& name, int day, int month, int year, const string& description, bool done);
     bool deleteElement(const string &deleteName);
     bool checkItem(const string& checkName);
